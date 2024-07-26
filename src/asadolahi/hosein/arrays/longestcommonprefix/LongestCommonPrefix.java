ab52563f256a377dbda12a78755552a0fdcb726c
Hosein <asadolahi.hosein226@gmail.com>
2024-07-26 22:50:53 +0330

    solved array#6

---
+++ src/asadolahi/hosein/arrays/longestcommonprefix/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+package asadolahi.hosein.arrays.longestcommonprefix;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        int min = Integer.MAX_VALUE;
+        for (String str : strs) {
+            if (str.length() < min) {
+                min = str.length();
+            }
+        }
+        int i;
+        for (i = 0; i < min; i++) {
+            char curr = strs[0].charAt(i);
+            for (String str : strs) {
+                if (curr != str.charAt(i)) {
+                    return str.substring(0, i);
+                }
+            }
+        }
+        return strs[0].substring(0, i);
+    }
+}
