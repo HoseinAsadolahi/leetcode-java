a6885e002bc85429d82281e826d4b33276fb8b4c
Hosein <asadolahi.hosein226@gmail.com>
2024-07-27 00:26:24 +0330

    solved array #9

---
+++ src/asadolahi/hosein/arrays/mergeintervals/MergeIntervals.java
@@ -0,0 +1,24 @@
+package asadolahi.hosein.arrays.mergeintervals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        List<int[]> out = new ArrayList<>();
+        int[] curr = intervals[0];
+        for (int[] interval: intervals) {
+            if (interval[0] > curr[1]) {
+                out.add(curr);
+                curr = interval;
+            } else {
+                curr[1] = Math.max(interval[1], curr[1]);
+            }
+        }
+        out.add(curr);
+        return out.toArray(new int[out.size()][]);
+    }
+}
