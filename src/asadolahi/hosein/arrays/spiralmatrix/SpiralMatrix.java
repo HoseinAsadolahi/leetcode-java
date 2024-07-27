5535ce5371fd8ee799ef145aa93191ccacdb302a
Hosein <asadolahi.hosein226@gmail.com>
2024-07-27 09:30:27 +0330

    update

--- src/asadolahi/hosein/arrays/spiralmatrix/SpiralMatrix.java
+++ src/asadolahi/hosein/arrays/spiralmatrix/SpiralMatrix.java
@@ -47,9 +47,4 @@ public class SpiralMatrix {
             }
         }
     }
-
-    public static void main(String[] args) {
-        int[][] x = {{1,2,3},{4,5,6},{7,8,9}};
-        System.out.println(spiralOrder(x));
-    }
 }
