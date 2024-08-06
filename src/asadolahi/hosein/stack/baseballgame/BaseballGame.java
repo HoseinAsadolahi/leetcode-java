7d255f4890412a8ec19fe6b3df666e8e491223b7
Hosein <asadolahi.hosein226@gmail.com>
2024-08-06 23:04:58 +0330

    solve stack#1

---
+++ src/asadolahi/hosein/stack/baseballgame/BaseballGame.java
@@ -0,0 +1,29 @@
+package asadolahi.hosein.stack.baseballgame;
+
+import java.util.Stack;
+
+public class BaseballGame {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+        for (String op : operations) {
+            switch (op) {
+                case "C":
+                    stack.pop();
+                    break;
+                case "D":
+                    stack.push(stack.peek() * 2);
+                    break;
+                case "+":
+                    stack.push(stack.peek() + stack.get(stack.size() - 2));
+                    break;
+                default:
+                    stack.push(Integer.parseInt(op));
+            }
+        }
+        int sum = 0;
+        for (Object integer : stack.toArray()) {
+            sum += (int) integer;
+        }
+        return sum;
+    }
+}
