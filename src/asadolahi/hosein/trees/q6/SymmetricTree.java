0a5e501f95e4641571c92fc44bd960487be10c78
Hosein <asadolahi.hosein226@gmail.com>
2024-09-10 12:06:36 +0330

    solve tree#6

---
+++ src/asadolahi/hosein/trees/q6/SymmetricTree.java
@@ -0,0 +1,38 @@
+package asadolahi.hosein.trees.q6;
+
+import asadolahi.hosein.trees.q5.SameTree;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        if (root.left == null && root.right == null) return true;
+        if (root.left == null || root.right == null) return false;
+        return isMirror(root.left, root.right);
+    }
+
+    public boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+        return isMirror(left.right, right.left) && isMirror(left.left, right.right);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
