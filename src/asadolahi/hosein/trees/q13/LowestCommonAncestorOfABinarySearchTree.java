98a318861f9dd9df2f415c25c31c310a2c9e0243
Hosein <asadolahi.hosein226@gmail.com>
2024-09-13 12:29:24 +0330

    solve tree#13

---
+++ src/asadolahi/hosein/trees/q13/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,33 @@
+package asadolahi.hosein.trees.q13;
+
+import asadolahi.hosein.trees.q12.ValidateBinarySearchTree;
+
+public class LowestCommonAncestorOfABinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
